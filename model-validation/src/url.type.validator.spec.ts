b7becea32fbafcf3748d19852fcab6b04b60ef5f
Daniel Schaffer <dan@dandoes.net>
2018-07-26 14:59:18 -0700

    add tests for `UrlTypeValidator` and `UuidTypeValidator`
    closes #6

---
+++ model-validation/src/url.type.validator.spec.ts
@@ -0,0 +1,36 @@
+import { expect } from 'chai';
+
+import { UrlTypeValidator } from './url.type.validator';
+
+// tslint:disable no-unused-expression
+describe('UrlTypeValidator', () => {
+
+    let validator: UrlTypeValidator;
+
+    beforeEach(() => {
+        validator = new UrlTypeValidator();
+    });
+    afterEach(() => {
+        validator = undefined;
+    });
+
+    describe('validate', () => {
+
+        it('returns a Url instance using the provided value', () => {
+
+            const urlStr = 'http://localhost/';
+            const result = validator.validate(urlStr);
+            expect(result.toString()).to.equal(urlStr);
+
+        });
+
+        it('throws if given an invalid url', () => {
+
+            const urlStr = '12345';
+            expect(() => validator.validate(urlStr)).to.throw;
+
+        });
+
+    });
+
+});
