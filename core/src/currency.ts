2238e8d7e0ace22f529196ed699ae43b5a13767b
Daniel Schaffer <dan@dandoes.net>
2018-08-07 21:02:48 -0700

    remove TODO; linting

--- core/src/currency.ts
+++ core/src/currency.ts
@@ -1,49 +1,45 @@
 const CURRENCY_PATTERN = /^(\D+)?(\d+\.\d{0,2})$/;
 
-// TODO: move to @dandi/core
 export class Currency extends Number {
+  public static parse(value: string): Currency {
+    const intValue = parseInt(value, 10);
 
-    public static parse(value: string): Currency {
-        const intValue = parseInt(value, 10);
-
-        if (!isNaN(intValue)) {
-            return new Currency(intValue);
-        }
-
-        const match = value.match(CURRENCY_PATTERN);
-        if (!match) {
-            return new Currency(intValue);
-        }
-        // TODO: get list of currency symbols, validate whether the symbol match is a valid currency symbol
-        return new Currency(parseInt(match[2], 10), match[1]);
-
+    if (!isNaN(intValue)) {
+      return new Currency(intValue);
     }
 
-    public get valid(): boolean {
-        return !isNaN(this.valueOf());
+    const match = value.match(CURRENCY_PATTERN);
+    if (!match) {
+      return new Currency(intValue);
     }
+    // TODO: get list of currency symbols, validate whether the symbol match is a valid currency symbol
+    return new Currency(parseInt(match[2], 10), match[1]);
+  }
 
-    constructor(value: number, public readonly code?: string) {
-        super(value);
+  public get valid(): boolean {
+    return !isNaN(this.valueOf());
+  }
+
+  constructor(value: number, public readonly code?: string) {
+    super(value);
+  }
+
+  public toString() {
+    return this.toDisplayString();
+  }
+
+  public toDisplayString(display: 'symbol' | 'code' | 'name' = 'symbol') {
+    return this.toLocaleString(null, {
+      style: 'currency',
+      currency: this.code,
+      currencyDisplay: display,
+    });
+  }
+
+  public equals(other: Number): boolean {
+    if (other instanceof Currency) {
+      return this.valueOf() === other.valueOf() && this.code === other.code;
     }
-
-    public toString() {
-        return this.toDisplayString();
-    }
-
-    public toDisplayString(display: 'symbol' | 'code' | 'name' = 'symbol') {
-        return this.toLocaleString(null, {
-            style: 'currency',
-            currency: this.code,
-            currencyDisplay: display,
-        });
-    }
-
-    public equals(other: Number): boolean {
-        if (other instanceof Currency) {
-            return this.valueOf() === other.valueOf() && this.code === other.code;
-        }
-        return this.valueOf() === other.valueOf();
-    }
-
+    return this.valueOf() === other.valueOf();
+  }
 }
