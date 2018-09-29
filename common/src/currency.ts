4ea646867a5fa1b8a1b9471cf3ad053f431a0244
Daniel Schaffer <dan@dandoes.net>
2018-09-29 10:59:29 -0700

    @dandi/common - make Currency cache values for reference comparison, similar to Uuid

--- common/src/currency.ts
+++ common/src/currency.ts
@@ -1,5 +1,7 @@
 const CURRENCY_PATTERN = /^(\D+)?(\d+\.\d{0,2})$/;
 
+const values = new Map<string, Currency>();
+
 export class Currency extends Number {
   public static parse(value: string): Currency {
     if (value === null || value === undefined) {
@@ -8,16 +10,33 @@ export class Currency extends Number {
     const intValue = parseInt(value, 10);
 
     if (!isNaN(intValue)) {
-      return new Currency(intValue);
+      return Currency.value(intValue);
     }
 
     const match = value.match(CURRENCY_PATTERN);
     if (!match) {
-      return new Currency(intValue);
+      return Currency.value(intValue);
     }
     // TODO: get list of currency symbols, validate whether the symbol match is a valid currency symbol
-    return new Currency(parseInt(match[2], 10), match[1]);
+    // TODO: support currencies that have a symbol after the value
+    return Currency.value(parseInt(match[2], 10), match[1]);
   }
+
+  public static value(amount: number, code?: string): Currency {
+    let key = amount.toString();
+    if (code) {
+      key += code;
+    }
+    const cachedValue = values.get(key);
+    if (cachedValue) {
+      return cachedValue;
+    }
+    const value = new Currency(amount, code);
+    values.set(key, value);
+    return value;
+  }
+
+  public static readonly zero = Currency.value(0);
 
   public get valid(): boolean {
     return !isNaN(this.valueOf());
@@ -25,6 +44,7 @@ export class Currency extends Number {
 
   constructor(value: number, public readonly code?: string) {
     super(value);
+    Object.freeze(this);
   }
 
   public toString() {
