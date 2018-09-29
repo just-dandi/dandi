793981174bf2249b7f2742f619a9c248bdc8222b
Daniel Schaffer <dan@dandoes.net>
2018-09-29 10:58:36 -0700

    @dandi/common - fix Uuid double-wrapping an existing Uuid value

--- common/src/uuid.ts
+++ common/src/uuid.ts
@@ -4,6 +4,9 @@ const UUID = new Map<string, Uuid>();
 
 export class Uuid extends String {
   public static for(value: string): Uuid {
+    if ((value as any) instanceof Uuid) {
+      return value;
+    }
     let uuid = UUID.get(value);
     if (!uuid) {
       uuid = new Uuid(value);
@@ -17,7 +20,7 @@ export class Uuid extends String {
   }
 
   public constructor(private readonly value?: string) {
-    super(value);
+    super((value as any) instanceof Uuid ? value.valueOf() : value);
     Object.freeze(this);
   }
 
