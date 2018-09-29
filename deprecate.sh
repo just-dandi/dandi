817a4fc8a2ba58b3381ae13635bc8b0346e66017
Daniel Schaffer <dan@dandoes.net>
2018-09-29 11:26:26 -0700

    deprecate old versions

--- deprecate.sh
+++ deprecate.sh
@@ -4,7 +4,7 @@ for dir in {'common','core','core-testing','model','model-validation','config','
 do
     pkgName=$(node -p "require('./${dir%*/}/package.json').name")
 
-    for pre in `seq 1 16`
+    for pre in `seq 16 22`
     do
       echo "${pkgName}@1.0.0-alpha.${pre}"
       npm deprecate "${pkgName}@1.0.0-alpha.${pre}" "outdated prerelease" --registry https://registry.npmjs.org/
