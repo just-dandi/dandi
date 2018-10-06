486c5cacb3cf8625856b7dffe87ca806eae7affb
Daniel Schaffer <dan@dandoes.net>
2018-10-06 09:40:09 -0700

    v1.0.0-alpha.28

--- unpublish.sh
+++ unpublish.sh
@@ -3,7 +3,7 @@
 for dir in {'common','core','core-node','core-testing','model','model-builder','config','config-aws-ssm','data','data-pg','cache','mvc','mvc-express','hal','mvc-hal','mvc-auth-firebase','aws-lambda-wrap'}
 do
     pkgName=$(node -p "require('./${dir%*/}/package.json').name")
-    targetPkg="${pkgName}@1.0.0-alpha.25"
+    targetPkg="${pkgName}@1.0.0-alpha.27"
     echo ${targetPkg}
     npm unpublish ${targetPkg} --registry https://registry.npmjs.org/
 
