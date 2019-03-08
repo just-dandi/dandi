89a1da1b8e20887b862ca0fc7d4f15c90d25027e
Daniel Schaffer <daniel.schaffer@sportsengine.com>
2019-03-08 08:44:02 -0800

    fix(dandi/core): replace `util` usage in `MissingProviderError`

--- packages/dandi/core/src/missing-provider-error.ts
+++ packages/dandi/core/src/missing-provider-error.ts
@@ -1,16 +1,14 @@
-import * as util from 'util'
-
-import { AppError } from '@dandi/common'
+import { AppError, CUSTOM_INSPECTOR } from '@dandi/common'
 
 import { InjectionToken, getTokenString } from './injection-token'
 import { InjectorContext } from './injector-context'
 
 export class MissingProviderError<T> extends AppError {
   constructor(public readonly token: InjectionToken<T>, private context: InjectorContext) {
-    super(`No provider for ${getTokenString(token)} while resolving ${context[util.inspect.custom]()}`)
+    super(`No provider for ${getTokenString(token)} while resolving ${context[CUSTOM_INSPECTOR]()}`)
   }
 
-  public [util.inspect.custom](): string {
-    return this.context[util.inspect.custom]()
+  public [CUSTOM_INSPECTOR](): string {
+    return this.context[CUSTOM_INSPECTOR]()
   }
 }
