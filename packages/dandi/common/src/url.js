dabcb20c413cb74a3ba09f8ba16ed0077f25ab5b
Daniel Schaffer <daniel.schaffer@sportsengine.com>
2019-09-02 13:58:35 -0700

    fix(dandi/common): url - correctly handle cases where `require.resolve('url')` returns something in a web environment

--- packages/dandi/common/src/url.js
+++ packages/dandi/common/src/url.js
@@ -1,6 +1,9 @@
 function getURL() {
   try {
-    if (require.resolve('url')) {
+    // require.resolve will return 'url' when executed by NodeJS
+    // for Webpack, it's possible that the "url" npm package (or an alias to it) could be installed, which is why the
+    // above check is required
+    if (require.resolve && require.resolve('url') === 'url') {
       return require('url').URL
     }
     return URL
