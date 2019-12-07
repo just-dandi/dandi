c6685337f4c505ae7eb37f443a0c8bb5e17ae267
Daniel Schaffer <daniel.schaffer@sportsengine.com>
2019-12-07 09:37:32 -0800

    chore(examples/simple-aws-lambda): create aws lambda example

---
+++ _examples/simple-aws-lambda/src/data-processor.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@dandi/core'
+
+import { ReceiveDataModel, ReceiveDataResponse } from './receive-data.model'
+
+const FAKE_LATENCY = 250
+
+@Injectable()
+export class DataProcessorService {
+
+  public processData(data: ReceiveDataModel): Promise<ReceiveDataResponse> {
+    return new Promise<ReceiveDataResponse>(resolve => {
+      setTimeout(() => resolve(this.convertData(data)), FAKE_LATENCY)
+    })
+  }
+
+  private convertData(data: ReceiveDataModel): ReceiveDataResponse {
+    return {
+      model: data,
+      modelType: data.constructor.name,
+    }
+  }
+
+}
