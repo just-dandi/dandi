c463477e22c4d371f26013826a5b57cd976a9fcf
Daniel Schaffer <dan@dandoes.net>
2020-01-20 17:43:42 -0800

    feat(dandi/common): improve AppError functionality and formatting - use standard indentation on multi-line messages and nested inner errors - add getInnerError utility method to find a type of error within the innerError stack - use the output of `AppError.stack` for `CUSTOM_INSPECTOR`

--- packages/dandi/common/src/app-error.ts
+++ packages/dandi/common/src/app-error.ts
@@ -1,18 +1,53 @@
+import { Constructor } from './constructor'
+import { CUSTOM_INSPECTOR } from './custom-inspector'
+
+export const BASE_SPACER = '    '
+
 export class AppError extends Error {
-  public static stack(err: Error): string {
-    return err instanceof AppError ? err.getStack() : err.stack
+  public static stack(err: Error, level: number = 1): string {
+    return err instanceof AppError ? err.getStack(level) : AppError.indent(err.stack, level)
+  }
+
+  public static getInnerError<TError extends Error>(errorType: Constructor<TError>, error: Error): TError {
+    if (error instanceof errorType) {
+      return error
+    }
+    if (error instanceof AppError) {
+      return AppError.getInnerError(errorType, error.innerError)
+    }
+
+    return undefined
+  }
+
+  private static indent(source: string, level: number): string {
+    if (!source) {
+      return source
+    }
+    const spacer = AppError.getIndent(level)
+    const [, linebreak] = source.match(/(\r?\n)/) || [undefined, '']
+    const lines = source.split(/\r?\n/)
+    return lines.join(`${linebreak}${spacer}`)
+  }
+
+  private static getIndent(level: number): string {
+    return Array(level + 1).join(BASE_SPACER)
   }
 
   constructor(message?: string, public readonly innerError?: Error) {
-    super(message)
+    super(AppError.indent(message, 0))
   }
 
-  public getStack(): string {
-    let stack = `${this.constructor.name} ${this.stack}`
+  public getStack(level: number = 1): string {
+    const spacer = AppError.getIndent(level)
+    let stack = AppError.indent(this.stack.replace(/^Error:/, `${this.constructor.name}:`), level)
     if (this.innerError) {
-      const innerStack = this.innerError instanceof AppError ? this.innerError.getStack() : this.innerError.stack
-      stack += '\n Inner ' + innerStack
+      const innerStack = AppError.stack(this.innerError, level)
+      stack += `\n\n${spacer}Inner ${innerStack}`
     }
     return stack
+  }
+
+  public [CUSTOM_INSPECTOR](): string {
+    return AppError.stack(this)
   }
 }
