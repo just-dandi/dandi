076b1d6115dc290216ccbd1e244a5b148bc61f76
Daniel Schaffer <dan@dandoes.net>
2020-02-05 08:37:07 -0800

    feat(dandi/mvc-view): support changes from @dandi/http-pipeline
    supports #28

--- packages/dandi/mvc-view/src/mvc-view-renderer.ts
+++ packages/dandi/mvc-view/src/mvc-view-renderer.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable, Injector, RestrictScope } from '@dandi/core'
 import { HttpRequestScope, MimeType } from '@dandi/http'
-import { HttpPipelineResult, HttpPipelineRendererBase, Renderer } from '@dandi/http-pipeline'
+import { HttpPipelineResult, HttpPipelineRendererBase, Renderer, isHttpPipelineDataResult } from '@dandi/http-pipeline'
 import { ViewResult } from '@dandi/mvc-view'
 
 import { ViewResultFactory } from './view-result-factory'
@@ -24,7 +24,7 @@ export class MvcViewRenderer extends HttpPipelineRendererBase {
 
     const factoryResult = await this.injector.inject(ViewResultFactory)
     const factory = factoryResult.singleValue
-    const viewResult = await factory(undefined, pipelineResult.data)
+    const viewResult = await factory(undefined, isHttpPipelineDataResult(pipelineResult) ? pipelineResult.data : {})
     return viewResult.value
   }
 }
