baa9d58e8efba4272148124442b493ad3bbf7ec8
Daniel Schaffer <dan@dandoes.net>
2020-02-16 13:08:28 -0800

    feat(dandi/common): add `parseBoolean` / `NaB` / `isNaB`

---
+++ packages/dandi/common/src/parse-boolean.ts
@@ -0,0 +1,38 @@
+import { globalSymbol } from './global.symbol'
+
+export type NaB = symbol & {
+  [Symbol.toStringTag]: () => '@dandi/common#NaB'
+}
+
+export const NaB: NaB = globalSymbol('NaB') as NaB
+
+export function parseBoolean(value: any): boolean | NaB {
+  switch (typeof value) {
+    case 'boolean': return value
+
+    case 'number':
+      if (value === 0) {
+        return false
+      }
+      if (value === 1) {
+        return true
+      }
+      return NaB
+
+    case 'string':
+      value = value.toLocaleLowerCase()
+      if (value === 'true') {
+        return true
+      }
+      if (value === 'false') {
+        return false
+      }
+      return NaB
+  }
+
+  return NaB
+}
+
+export function isNaB(value: any): value is NaB {
+  return value === NaB
+}
