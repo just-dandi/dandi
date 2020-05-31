22bdb5b6efed61b60e972b9ae4cc4f06c245d880
Daniel Schaffer <dan@dandoes.net>
2020-05-31 15:17:24 -0700

    fix(dandi/http-pipeline): correct typeof CorsConfig.allowHeaders to HttpRequestHeader[]

--- packages/dandi/http-pipeline/src/cors/cors-config.ts
+++ packages/dandi/http-pipeline/src/cors/cors-config.ts
@@ -1,11 +1,11 @@
-import { HttpResponseHeader } from '@dandi/http'
+import { HttpRequestHeader, HttpResponseHeader } from '@dandi/http'
 
 import { CorsAllowOriginFn } from './cors'
 import { CorsOriginWhitelist } from './cors-origin-whitelist'
 
 export interface CorsConfig {
   allowCredentials?: true
-  allowHeaders?: HttpResponseHeader[]
+  allowHeaders?: HttpRequestHeader[]
   allowOrigin?: CorsOriginWhitelist | CorsAllowOriginFn
   exposeHeaders?: HttpResponseHeader[]
   maxAge?: number
