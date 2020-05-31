4377809c897881516a9341c68f3bce6a295a3327
Daniel Schaffer <dan@dandoes.net>
2020-05-31 15:18:39 -0700

    fix(dandi/mvc): prevent AuthorizationAuthProviderFactory logic attempting to authenticate on OPTIONS requests

--- packages/dandi/mvc/src/authorization-auth-provider.factory.ts
+++ packages/dandi/mvc/src/authorization-auth-provider.factory.ts
@@ -1,42 +1,66 @@
-import { Injectable, Provider, Injector } from '@dandi/core'
-import { HttpRequest, UnauthorizedError } from '@dandi/http'
+import { Injectable, InjectionToken, Injector, Provider } from '@dandi/core'
+import {
+  HttpHeader,
+  HttpMethod,
+  HttpRequest,
+  HttpRequestHeadersAccessor,
+  HttpRequestScope,
+  UnauthorizedError,
+} from '@dandi/http'
 import { HttpPipelinePreparer } from '@dandi/http-pipeline'
 
 import { AuthProviderFactory } from './auth-provider.factory'
 import { AuthorizationService } from './authorization.service'
 import { AuthorizedUserProvider } from './authorized.user'
+import { localOpinionatedToken } from './local.token'
 import { RequestAuthorizationService } from './request-authorization.service'
 import { Route } from './route'
+
+export const AuthorizationScheme: InjectionToken<string> = localOpinionatedToken<string>('AuthorizationScheme', {
+  multi: false,
+  restrictScope: HttpRequestScope,
+})
+
+function authorizationSchemeFactory(headers: HttpRequestHeadersAccessor, route: Route): string {
+  const authHeader = headers.get(HttpHeader.authorization)
+  if (!authHeader && route.authorization) {
+    throw new UnauthorizedError()
+  }
+  const authSchemeEndIndex = authHeader.indexOf(' ')
+  return authHeader.substring(0, authSchemeEndIndex > 0 ? authSchemeEndIndex : undefined)
+}
+
+export const AuthorizationSchemeProvider: Provider<string> = {
+  provide: AuthorizationScheme,
+  useFactory: authorizationSchemeFactory,
+  deps: [HttpRequestHeadersAccessor, Route],
+}
 
 @Injectable(AuthProviderFactory)
 @HttpPipelinePreparer()
 export class AuthorizationAuthProviderFactory implements AuthProviderFactory {
 
   public generateAuthProviders(route: Route, req: HttpRequest): Provider<any>[] {
-    const authHeader = req.get('Authorization')
 
-    if (!authHeader) {
-      if (route.authorization) {
-        throw new UnauthorizedError()
-      }
-
+    if (req.method === HttpMethod.options) {
       return []
     }
 
-    const authSchemeEndIndex = authHeader.indexOf(' ')
-    const authScheme = authHeader.substring(0, authSchemeEndIndex > 0 ? authSchemeEndIndex : undefined)
-    async function authServiceResultFactory(injector: Injector): Promise<any> {
+    async function authServiceResultFactory(authScheme: string, injector: Injector): Promise<AuthorizationService> {
       return (await injector.inject(AuthorizationService(authScheme), !route.authorization))?.singleValue
     }
     const providers: Provider<any>[] = [
       {
         provide: RequestAuthorizationService,
         useFactory: authServiceResultFactory,
+        restrictScope: HttpRequestScope,
         deps: [
+          AuthorizationScheme,
           Injector,
         ],
       },
       AuthorizedUserProvider,
+      AuthorizationSchemeProvider,
     ]
 
     if (Array.isArray(route.authorization) && route.authorization.length) {
