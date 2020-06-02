1f684d33c3df9a9194b0a9a7acad76b9c69cfa7d
Daniel Schaffer <dan@dandoes.net>
2020-06-02 16:06:25 -0700

    feat(dandi/common): getMetadata - ignore metadata stored on super classes by default

--- packages/dandi/common/src/metadata.ts
+++ packages/dandi/common/src/metadata.ts
@@ -1,7 +1,15 @@
 export type MetadataAccessor<TMeta> = <T>(target: any) => TMeta
 
-export function getMetadata<T>(key: symbol, init: () => T, target: any): T {
-  let meta: T = Reflect.get(target, key)
+/**
+ * @internal
+ *
+ * @param key
+ * @param init
+ * @param target
+ * @param allowSuper controls whether existing metadata can be retrieved from a super class
+ */
+export function getMetadata<T>(key: symbol, init: () => T, target: any, allowSuper: boolean = false): T {
+  let meta: T = (allowSuper || target.hasOwnProperty(key)) ? Reflect.get(target, key) : undefined
   if (!meta) {
     meta = init ? init() : ({} as T)
     Reflect.set(target, key, meta)
