241ee92321eee86a0786255189f8e8af92a7e8e5
Daniel Schaffer <dan@dandoes.net>
2020-06-02 16:08:04 -0700

    fix(dandi/hal): stop using class name-based symbols because they break when mangled (e.g. with Terser / Uglify)

--- packages/dandi/hal/src/relations.decorator.ts
+++ packages/dandi/hal/src/relations.decorator.ts
@@ -1,21 +1,20 @@
 import { Constructor } from '@dandi/common'
 
-import { ResourceMetadata, getResourceMetadata, resourceMetaKey } from './resource.metadata'
+import { ResourceMetadata, getResourceMetadata, RESOURCE_META_KEY } from './resource.metadata'
 
-export function Relations(forModel: Constructor<any>): any { // use any return type because Constructor<any> is no assignable to TFunction?
-  return function(target: Constructor<any>): void {
+export function Relations(forModel: Constructor): any { // use any return type because Constructor<any> is no assignable to TFunction?
+  return function(target: Constructor): void {
     // super simple hack: use an object reference to link the target's relation metadata to the decorated class
-    const key = resourceMetaKey(target)
-    const existingMeta = Reflect.get(target, key) as ResourceMetadata
+    const existingMeta = Reflect.get(target, RESOURCE_META_KEY) as ResourceMetadata
     const meta = getResourceMetadata(forModel)
     if (existingMeta) {
-      // apparently member decorators run before class decorators, so we need to copy over any existing relation metadata
+      // member decorators run before class decorators, so we need to copy over any existing relation metadata
       Object.keys(existingMeta.relations).forEach((rel) => {
         if (existingMeta.relations[rel] && existingMeta.relations[rel].resource) {
           meta.relations[rel] = Object.assign(meta.relations[rel] || {}, existingMeta.relations[rel])
         }
       })
     }
-    Reflect.set(target, key, meta)
+    Reflect.set(target, RESOURCE_META_KEY, meta)
   }
 }
