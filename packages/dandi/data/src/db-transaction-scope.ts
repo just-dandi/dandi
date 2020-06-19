97294fdb0e6eb11eae5aba65984e60bc85a4d0f1
Daniel Schaffer <dan@dandoes.net>
2020-06-19 15:50:41 -0700

    feat(dandi/data): add DbTransactionScope, add DbTransactionScope as scope restriction to DbTransactionClient
    BREAKING: injecting DbTransactionClient now requires using DbTransactionScope
    
    supports #74

---
+++ packages/dandi/data/src/db-transaction-scope.ts
@@ -0,0 +1,21 @@
+import { CustomInjectionScope } from '@dandi/core'
+
+const DB_TRANSACTION_SCOPE = '@dandi/data#DbTransaction'
+
+export interface DbTransactionScope extends CustomInjectionScope {
+  description: '@dandi/data#DbTransaction'
+}
+
+export interface DbTransactionScopeInstance extends DbTransactionScope {
+  instanceId: any
+}
+
+export const DbTransactionScope: DbTransactionScope = {
+  description: DB_TRANSACTION_SCOPE,
+  type: Symbol.for(DB_TRANSACTION_SCOPE),
+}
+
+let instanceId: number = 0
+export function createDbTransactionScope(): DbTransactionScopeInstance {
+  return Object.assign({ instanceId: instanceId++ }, DbTransactionScope)
+}
