f95d69797873ff13a9843e7234ae7f903ff1b716
Daniel Schaffer <dan@dandoes.net>
2020-06-19 14:37:33 -0700

    fix(dandi/http-pipeline): NativeJsonObjectRenderer - use getErrorRendererData to render errors
    fixes #73

--- packages/dandi/http-pipeline/src/rendering/native-json-object-renderer.ts
+++ packages/dandi/http-pipeline/src/rendering/native-json-object-renderer.ts
@@ -1,5 +1,8 @@
+import { Inject } from '@dandi/core'
 import { MimeType } from '@dandi/http'
 
+import { HttpPipelineConfig } from '../http-pipeline-config'
+import { HttpPipelineErrorRendererDataFactory, isHttpPipelineErrorResult } from '../http-pipeline-error-result'
 import { HttpPipelineResult, isHttpPipelineDataResult } from '../http-pipeline-result'
 
 import { HttpPipelineRendererBase } from './http-pipeline-renderer-base'
@@ -10,11 +13,20 @@ export class NativeJsonObjectRenderer extends HttpPipelineRendererBase {
 
   protected readonly defaultContentType: string = MimeType.applicationJson
 
-  constructor() {
+  constructor(
+    @Inject(HttpPipelineConfig) private config: HttpPipelineConfig,
+  ) {
     super()
   }
 
   protected renderPipelineResult(contentType: string, pipelineResult: HttpPipelineResult): string | Promise<string> {
+    if (isHttpPipelineErrorResult(pipelineResult)) {
+      const dataFactory = pipelineResult.data instanceof HttpPipelineErrorRendererDataFactory ?
+        pipelineResult.data :
+        new HttpPipelineErrorRendererDataFactory(pipelineResult)
+      return JSON.stringify(dataFactory.getErrorRendererData(this.config.debugMode))
+    }
+
     if (isHttpPipelineDataResult(pipelineResult)) {
       return JSON.stringify(pipelineResult.data)
     }
