f10119cdef2033c481aefba07e79b7fae9b00fc1
Daniel Schaffer <dan@dandoes.net>
2020-07-28 11:59:08 -0700

    chore(dandi/mvc-express): use localTokenFactory

--- packages/dandi-contrib/mvc-express/src/express-instance.ts
+++ packages/dandi-contrib/mvc-express/src/express-instance.ts
@@ -1,10 +1,10 @@
-import { InjectionToken, Provider } from '@dandi/core'
+import { Provider } from '@dandi/core'
 import { Express } from 'express'
 import * as express from 'express'
 
 import { localToken } from './local-token'
 
-export const ExpressInstance: InjectionToken<Express> = localToken.opinionated<Express>('Express', {
+export const ExpressInstance = localToken.opinionated<Express>('Express', {
   multi: false,
 })
 
--- packages/dandi-contrib/mvc-express/src/express-mvc-config.ts
+++ packages/dandi-contrib/mvc-express/src/express-mvc-config.ts
@@ -1,14 +1,9 @@
-import { InjectionToken } from '@dandi/core'
-
 import { localToken } from './local-token'
 
 export interface ExpressMvcConfig {
   port: number
 }
 
-export const ExpressMvcConfig: InjectionToken<ExpressMvcConfig> = localToken.opinionated<ExpressMvcConfig>(
-  '@dandi/mvc:ExpressMvcConfig',
-  {
-    multi: false,
-  },
-)
+export const ExpressMvcConfig = localToken.opinionated<ExpressMvcConfig>('@dandi/mvc:ExpressMvcConfig', {
+  multi: false,
+})
