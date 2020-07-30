a063fd7307cf0c32c42314d475d2b8ff54f08f62
Daniel Schaffer <dan@dandoes.net>
2020-07-29 18:51:47 -0700

    fix(dandi-contrib/sentry-mvc-express): SentryMvcScopePreparer - refactor to handle no auth user on routes requiring auth (e.g. options, requests that would result in a 403)

--- packages/dandi-contrib/sentry-mvc-express/src/sentry-mvc-scope-preparer.ts
+++ packages/dandi-contrib/sentry-mvc-express/src/sentry-mvc-scope-preparer.ts
@@ -1,23 +1,43 @@
 import { SentryClient, SentryScopeData, SentryScopeDataFragment } from '@dandi-contrib/sentry'
-import { Inject } from '@dandi/core'
+import { Inject, Injectable, RestrictScope, ScopeBehavior, Optional, Provider } from '@dandi/core'
+import { HttpRequestScope } from '@dandi/http'
 import { HttpPipelinePreparer, HttpPipelinePreparerResult } from '@dandi/http-pipeline'
 import { AuthorizedUser, Route } from '@dandi/mvc'
 
-async function sentryMvcScopeDataFactory(user: AuthorizedUser, route: Route): Promise<SentryScopeData> {
-  return {
-    tags: {
-      'route.method': route.httpMethod,
-      'route.path': route.path,
-      'route.controller': route.controllerCtr.name,
-      'route.controllerMethod': route.controllerMethod.toString(),
-      'route.hasCors': route.cors ? 'yes' : 'no',
-      'route.requiresAuth': route.authorization ? 'yes' : 'no',
-    },
-    user: user
-      ? {
-          id: user.uid,
-        }
-      : undefined,
+@Injectable(RestrictScope(ScopeBehavior.perInjector(HttpRequestScope)))
+class SentryMvcScopeDataService {
+  public static factory(scopeData: SentryMvcScopeDataService): SentryScopeData {
+    return scopeData.getData()
+  }
+
+  public static readonly provider: Provider<SentryScopeData> = {
+    provide: SentryScopeDataFragment,
+    useFactory: SentryMvcScopeDataService.factory,
+    deps: [SentryMvcScopeDataService],
+    providers: [SentryMvcScopeDataService],
+  }
+
+  constructor(
+    @Inject(Route) private readonly route: Route,
+    @Optional() @Inject(AuthorizedUser) private readonly authUser: AuthorizedUser,
+  ) {}
+
+  public getData(): SentryScopeData {
+    return {
+      tags: {
+        'route.method': this.route.httpMethod,
+        'route.path': this.route.path,
+        'route.controller': this.route.controllerCtr.name,
+        'route.controllerMethod': this.route.controllerMethod.toString(),
+        'route.hasCors': this.route.cors ? 'yes' : 'no',
+        'route.requiresAuth': this.route.authorization ? 'yes' : 'no',
+      },
+      user: this.authUser
+        ? {
+            id: this.authUser.uid,
+          }
+        : undefined,
+    }
   }
 }
 
@@ -26,13 +46,6 @@ export class SentryMvcScopePreparer implements HttpPipelinePreparer {
   constructor(@Inject(SentryClient) private readonly sentry: SentryClient) {}
 
   public async prepare(): Promise<HttpPipelinePreparerResult> {
-    return [
-      {
-        provide: SentryScopeDataFragment,
-        useFactory: sentryMvcScopeDataFactory,
-        deps: [AuthorizedUser, Route],
-        async: true,
-      },
-    ]
+    return [SentryMvcScopeDataService.provider]
   }
 }
